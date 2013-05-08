250612ab1839d61d25238b0b70cf9a4696647dc0
Darcy Laycock <sutto@sutto.net>
2013-05-08 20:13:16 +0800

    Lazy load, so the cexts aren't loaded every time.

--- lib/emmett.rb
+++ lib/emmett.rb
@@ -1,6 +1,9 @@
 require "emmett/version"
 
 module Emmett
-  require 'emmett/document_manager'
+
+  # Autoload, so we don't auto pull in the required libraries.
+  autoload :DocumentManager, 'emmett/document_manager'
+
   require 'emmett/railtie' if defined?(::Rails::Railtie)
 end
